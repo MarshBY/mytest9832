13f15544a988ab5d9858673da557b30187be3c58
Marcel <marshby@gmail.com>
2020-10-31 18:29:06 +0100

    Fifth

--- main.js
+++ main.js
@@ -37,4 +37,5 @@ app.post("/todos", async (req, res) => {
 })
 
 //Listen
-app.listen(3000, () => console.log("Server listening on port 3000"));
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
