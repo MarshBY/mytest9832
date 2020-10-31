f985ac7e77b6f9cb5bce7f4a953f80577ac5cf58
Marcel <marshby@gmail.com>
2020-10-31 18:45:43 +0100

    DB

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "123Marcel.",
-  host: "localhost",
+  user: "lgjystzhtaqszw",
+  password: "90dd797f6fc228d241628a83ae213b1caed4be5f531ceb6d01a7cf65a61d25f1",
+  host: "ec2-54-246-115-40.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  database: "pern"
+  database: "denb2rkfmgodr5"
 });
 
 module.exports = pool;
